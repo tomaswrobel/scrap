431eac49568bc07977241d054507825a5bbdc3b2
Tomáš Wróbel <mail@tomaswrobel.dev>
2025-08-17 03:12:05 +0200

    Add the web

---
+++ web/astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
+
+// https://astro.build/config
+export default defineConfig({
+	adapter: vercel()
+});
