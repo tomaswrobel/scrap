f808a8580adbd8e190be5768892c2eab90548d2a
Tomáš Wróbel <mail@tomaswrobel.dev>
2025-08-21 19:39:49 +0200

    v7: fix base url

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ const host = process.env.TAURI_DEV_HOST;
 export default defineConfig({
 	clearScreen: false,
 	publicDir: "node_modules/blockly/media",
+	base: "./",
 	server: {
 		hmr: host
 			? {
